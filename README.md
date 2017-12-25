b515583b1ef8cf06e8f61f2d253bd90d5ba4f4d3
Sergey Morozov <sergeymorozov@MacBook-Pro-Sergey.local>
2017-12-25 21:49:39 +0300

    выполнен основной функционал

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Count and show most frequent words in a file
+
+# Quickstart
+
+Run this script with Python 3.x. As a parameter you have to specify path to file with words to count
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python lang_frequency.py <path to file>
+
+4665:и
+2178:в
+1869:не
+1624:что
+1531:на
+1435:он
+1354:с
+952:как
+866:к
+825:его
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,32 @@
+from collections import Counter
+import sys
+import re
 
 
-def load_data(filepath):
-    pass
+def load_data(filepath, chunk_size=1024):
+    with open(filepath, "r", encoding="utf-8") as file:
+        while True:
+            text_peace = file.read(chunk_size)
+            if not text_peace:
+                break
+            yield text_peace
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(filepath, words_count):
+    c = Counter()
+    for peace in load_data(filepath):
+        c.update(re.findall(r"(\w+)", peace))
+    result = c.most_common()[:words_count]
+    print_result(result)
 
 
-if __name__ == '__main__':
-    pass
+def print_result(result):
+    for i in range(0, len(result)):
+        print(str(result[i][1]) + ":" + str(result[i][0]))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        get_most_frequent_words(sys.argv[1], 10)
+
+
