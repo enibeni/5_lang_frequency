fad978d1d99132cc4fdc6d538642b78369a04ee8
Sergey Morozov <sergeymorozov@MacBook-Pro-Sergey.local>
2017-12-27 22:03:28 +0300

    считывание файла полностью а не кусками

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,34 +3,34 @@ import sys
 import re
 
 
-WORDS_COUNT_TO_SHOW = 10
-
-
-def load_data(filepath, chunk_size=1024):
+def load_data(filepath):
     with open(filepath, "r", encoding="utf-8") as file:
-        while True:
-            text_peace = file.read(chunk_size)
-            if not text_peace:
-                break
-            yield text_peace
+            text = file.read()
+            return text
 
 
-def get_most_frequent_words(filepath, words_count):
+def find_most_frequent_words(text, words_count_to_show):
     c = Counter()
-    for peace in load_data(filepath):
-        peace = peace.lower()
-        c.update(re.findall(r"(\w+)", peace))
-    frequent_words = c.most_common()[:words_count]
-    print_result(frequent_words)
+    for line in text.split("\n"):
+        line = line.lower()
+        c.update(re.findall(r"(\w+)", line))
+    frequent_words = c.most_common(words_count_to_show)
+    return frequent_words
 
 
 def print_result(frequent_words):
-    for index in range(len(frequent_words)):
-        print(str(frequent_words[index][1]) + ":" + str(frequent_words[index][0]))
+    for word in frequent_words:
+        print("{} : {}".format(word[0], word[1]))
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        get_most_frequent_words(sys.argv[1], WORDS_COUNT_TO_SHOW)
+        text = load_data(sys.argv[1])
+        frequent_words = \
+            find_most_frequent_words(text,
+                                     words_count_to_show=10)
+        print_result(frequent_words)
+    else:
+        print("enter path to a file")
 
 
