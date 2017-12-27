a3e7e247a7514473398bdad84d65a4bbf7695cae
Sergey Morozov <sergeymorozov@MacBook-Pro-Sergey.local>
2017-12-28 00:10:02 +0300

    использован tupleunpacking

--- lang_frequency.py
+++ lang_frequency.py
@@ -5,32 +5,27 @@ import re
 
 def load_data(filepath):
     with open(filepath, "r", encoding="utf-8") as file:
-            text = file.read()
-            return text
+        text = file.read()
+        return text
 
 
 def find_most_frequent_words(text, words_count_to_show):
     c = Counter()
-    for line in text.split("\n"):
-        line = line.lower()
-        c.update(re.findall(r"(\w+)", line))
+    c.update(re.findall(r"(\w+)", text.lower()))
     frequent_words = c.most_common(words_count_to_show)
     return frequent_words
 
 
 def print_result(frequent_words):
-    for word in frequent_words:
-        print("{} : {}".format(word[0], word[1]))
+    for (word, count) in frequent_words:
+        print("{} : {}".format(word, count))
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         text = load_data(sys.argv[1])
-        frequent_words = \
-            find_most_frequent_words(text,
-                                     words_count_to_show=10)
+        frequent_words = find_most_frequent_words(text,
+                                                  words_count_to_show=10)
         print_result(frequent_words)
     else:
         print("enter path to a file")
-
-
